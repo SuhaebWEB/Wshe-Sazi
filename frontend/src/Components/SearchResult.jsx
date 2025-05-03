4147be8ed8a5ad862e9a91eda60062fd64721a33
SuhaebWEB <Suhaeb.Fazel03@gmail.com>
2025-05-03 15:15:03 +0300

    Update SearchResult.jsx

--- frontend/src/Components/SearchResult.jsx
+++ frontend/src/Components/SearchResult.jsx
@@ -9,7 +9,7 @@ const Searched = ({getData}) => {
                         getData.slice(0, 10).map((result, index) => (
 
                             <div key={index} className=" w-full flex flex-col items-center gap-3.5 1.5xl:px-6 1.5xl:items-end ">
-                                <h3 className=" mt-3" style={{ fontSize: "34px" }}>{result.kurdish_word.kurdish_word}</h3>
+                                <h3 className="max-1.5xl:text-center text-right mt-3" style={{ fontSize: "34px" }}>{result.kurdish_word.kurdish_word}</h3>
                                 <p className=" max-1.5xl:text-center text-right " style={{ fontSize: "20px" }} > {result.kurdish_word.reason} </p>
                                 <div className="h-[1px] w-full bg-gradient-to-r from-[rgba(0,0,0,0)] from-20% via-black to-[rgba(0,0,0,0)] to-80%"></div>
                             </div>
