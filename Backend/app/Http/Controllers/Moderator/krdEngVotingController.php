ada6656f187cc473d582b8d82b41085d4ab1bdbf
SuhaebWEB <Suhaeb.Fazel03@gmail.com>
2025-05-10 11:06:34 +0300

    Update krdEngVotingController.php

--- Backend/app/Http/Controllers/Moderator/krdEngVotingController.php
+++ Backend/app/Http/Controllers/Moderator/krdEngVotingController.php
@@ -19,6 +19,7 @@ class krdEngVotingController extends Controller
         })
         ->select('id','english_word', 'kurdish_word_id')
         ->with('kurdish_word:id,kurdish_word,reason')
+        ->orderBy('english_word', 'desc')
         ->get()->groupBy('english_word');
 
         
